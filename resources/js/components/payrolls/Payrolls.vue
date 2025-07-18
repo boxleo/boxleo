a58c875d43b5eb00d2fb1d422dc947cbf4eedeac
JasmineMwangi <jasminemaurine1@gmail.com>
2025-07-18 16:53:41 +0300

    amount prefix update

--- resources/js/components/payrolls/Payrolls.vue
+++ resources/js/components/payrolls/Payrolls.vue
@@ -410,7 +410,7 @@
                     label="Basic Pay"
                     type="number"
                     variant="outlined"
-                    prefix="$"
+                    prefix="Ksh"
                     :rules="[v => !!v || 'Basic pay is required']"
                     @input="calculatePayrollTotals"
                     required
@@ -487,7 +487,7 @@
                     type="number"
                     variant="outlined"
                     density="comfortable"
-                    prefix="$"
+                    prefix="ksh"
                     hide-details
                     @input="calculatePayrollTotals"
                   ></v-text-field>
@@ -536,7 +536,7 @@
                     type="number"
                     variant="outlined"
                     density="comfortable"
-                    prefix="$"
+                    prefix="ksh"
                     hide-details
                     @input="calculatePayrollTotals"
                   ></v-text-field>
